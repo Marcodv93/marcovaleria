6beec17812e8ee84ace2c67683bad79e2edb1ade
Marcodv93 <marcodv93@gmail.com>
2017-09-03 20:27:10 +0200

    i changed googlesearchfunction in a function for a query

--- googlesearchfunction.py
+++ googlesearchfunction.py
@@ -3,16 +3,19 @@ from urllib import request
 import requests
 from bs4 import BeautifulSoup
 
-url="https://www.google.be/search?tbm=isch&q=papa%20francisco"
-r=requests.get(url)
-sauce=r.content
-soup=BeautifulSoup(sauce, "html.parser")
-myimages=soup.findAll('img')
+def google_search(query):
+	query = query.replace(' ', '%20')
+	url="https://www.google.be/search?tbm=isch&q="+str(query)
+	r=requests.get(url)
+	sauce=r.content
+	soup=BeautifulSoup(sauce, "html.parser")
+	myimages=soup.findAll('img')
 
-i=0
-for image in myimages:
-    i+=1
-    name='image'+str(i)+'.jpg'
-    request.urlretrieve(str(image['src']),name)
-    if i>2:
-    break
+	i=0
+	for image in myimages:
+	    i+=1
+	    name=query+str(i)+'.jpg'
+	    request.urlretrieve(str(image['src']),name)
+	    if i>2:
+	    	break
+google_search('tortilla')
