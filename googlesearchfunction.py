97c1bae920abc9a10d1086df310406565b82c2bd
marcodallavecchia <marcodv93@gmail.com>
2019-11-13 19:18:17 +0100

    changed googleserachfunction.py and added TIFs images

--- googlesearchfunction.py
+++ googlesearchfunction.py
@@ -3,15 +3,16 @@
 from urllib import request
 import requests
 from bs4 import BeautifulSoup
+import numpy as np
 
 def google_search(query):
 	query = query.replace(' ', '%20')
-	url="https://www.google.be/search?tbm=isch&q="+str(query)
+	url="https://www.google.be/"+str(query)
 	r=requests.get(url)
 	sauce=r.content
 	soup=BeautifulSoup(sauce, "html.parser")
 	myimages=soup.findAll('img')
-
+	print(url)
 	i=0
 	for image in myimages:
 	    i+=1
@@ -19,6 +20,9 @@ def google_search(query):
 	    request.urlretrieve(str(image['src']),name)
 	    if i>2:
 	    	break
-google_search('cutie pie')
+google_search('ugly spy')
+
+def print_function(inputstring):
+	print(inputstring)
 
 # this is Marco modificationssss
