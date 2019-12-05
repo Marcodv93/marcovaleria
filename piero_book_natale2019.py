092e39a6bc101bb8be9fde992693bdcc645e113b
Marco Dalla Vecchia <marco.dallavecchia@kuleuven.be>
2019-12-05 22:10:17 +0100

    implemented page change with buttons

--- piero_book_natale2019.py
+++ piero_book_natale2019.py
@@ -2,13 +2,13 @@
 
 
 import tkinter as tk
-
+import os
 
 app = tk.Tk()
 app.geometry('1600x900') 
 
 class frame_master(tk.Frame):
-    def __init__(self, leftpage_text, rightpage_text):
+    def __init__(self, pages_dictionary):
         tk.Frame.__init__(self, bg='black', height=900, width=1600)
         self.place(x=0,y=0)
 
@@ -18,15 +18,20 @@ class frame_master(tk.Frame):
         frame_rightpage = tk.Frame(self, height=800, width=800, bg='blue')
         frame_rightpage.place(x=800,y=0)
 
-        label_leftpage = tk.Label(frame_leftpage, text=leftpage_text)
-        label_leftpage.place(relx=0.5,rely=0.5)
-        label_rightpage = tk.Label(frame_rightpage, text=rightpage_text)
-        label_rightpage.place(relx=0.5,rely=0.5)
+        tkvar_left_text = tk.StringVar()
+        tkvar_left_text.set(pages_dictionary[1])
+        label_leftpage = tk.Label(frame_leftpage, textvariable=tkvar_left_text, width=70, height=35, font=("Helvetica", 14), wraplength = 700)
+        label_leftpage.place(x=0,y=0)
+
+        tkvar_right_text = tk.StringVar()
+        tkvar_right_text.set(pages_dictionary[2])
+        label_rightpage = tk.Label(frame_rightpage, textvariable=tkvar_right_text, width=70, height=35,font=("Helvetica", 14), wraplength = 700)
+        label_rightpage.place(x=0,y=0)
 
         frame_controls = tk.Frame(self, height=100, width=1600, bg="green")
         frame_controls.place(x=0, y=800)
 
-        button_previous = tk.Button(frame_controls, text ="Previous Page", command=lambda: self.decrease_page(self.page_counter_left, self.page_counter_right))
+        button_previous = tk.Button(frame_controls, text ="Previous Page", command=lambda: self.decrease_page(tkvar_left_text, tkvar_right_text))
         button_previous.place(x=10, y=25,relheight=0.5, relwidth=0.2)
 
         self.page_counter_left = tk.IntVar()
@@ -38,28 +43,35 @@ class frame_master(tk.Frame):
         right_counter = tk.Label(frame_controls, textvariable=self.page_counter_right)
         right_counter.place(x=1200,y=25)
 
-        button_next = tk.Button(frame_controls, text ="Next Page", command=lambda: self.increase_page(self.page_counter_left, self.page_counter_right))
+        button_next = tk.Button(frame_controls, text ="Next Page", command=lambda: self.increase_page(tkvar_left_text, tkvar_right_text))
         button_next.place(anchor = "ne", x=1590, y=25, relheight=0.5, relwidth=0.2)
-    def increase_page(self, page_counter_left,page_counter_right):
-        page_counter_left.set(page_counter_left.get()+2)
-        page_counter_right.set(page_counter_right.get()+2)
-    def decrease_page(self, page_counter_left, page_counter_right):
-        if page_counter_left.get()>2:
-            page_counter_left.set(page_counter_left.get()-2)
-            page_counter_right.set(page_counter_right.get()-2)
-        
+    def increase_page(self, tkvar_left_text, tkvar_right_text):
+        if self.page_counter_right.get() < len(pages_dictionary.keys()):
+            self.page_counter_left.set(self.page_counter_left.get()+2)
+            self.page_counter_right.set(self.page_counter_right.get()+2)
+            self.update_pages(tkvar_left_text, tkvar_right_text)
+    def decrease_page(self, tkvar_left_text, tkvar_right_text):
+        if self.page_counter_left.get()>2:
+            self.page_counter_left.set(self.page_counter_left.get()-2)
+            self.page_counter_right.set(self.page_counter_right.get()-2)
+            self.update_pages(tkvar_left_text, tkvar_right_text)
+    def update_pages(self, tkvar_left_text, tkvar_right_text):
+        tkvar_left_text.set(pages_dictionary[self.page_counter_left.get()])
+        tkvar_right_text.set(pages_dictionary[self.page_counter_right.get()])
+
 
 pages_dictionary = {}
-for page_number in range(1, 5):
-    if page_number%2 == 0:
-        pages_dictionary[page_number] = "RIGHT" + str(page_number)
-    else:
-        pages_dictionary[page_number] = "LEFT" + str(page_number) 
+path = "D:\\Documents\\Programming\\Python Programming\\marcovaleria"+'\\story\\'
+for file in os.listdir(path):
+    page_number = int(file.split("page")[-1].split(".txt")[0])
+    current_file = open(path+file, "r")
+    pages_dictionary[page_number] = current_file.read()
 
 class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
         #tk.Frame.__init__(self, *args, **kwargs)
-        frame = frame_master(pages_dictionary[1],pages_dictionary[2])
+        frame = frame_master(pages_dictionary)
+
 
 
           
